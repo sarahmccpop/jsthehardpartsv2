bf8f41837496ba0c8238e276337e01b455ee6450
Sarah McConville <sarah.mcconville@wegotpop.com>
2021-10-25 12:05:36 +0100

    few extra details

--- datetime.js
+++ datetime.js
@@ -1,31 +1,45 @@
 
-const d = new Date();
+
 const calltime = new Date(2021, 9, 25, 8, 0, 0, 0);
-const wrap = new Date(2021,9, 25, 19, 0, 0, 0 )
+const wrap = new Date(2021,9, 25, 19, 0, 0, 0 );
 const beganWork = new Date(2021, 9, 25, 7, 30, 0, 0);
 const finishedWork = new Date(2021, 9, 25, 20, 0, 0, 0);
+const expectedWrap = new Date(2021,9, 25, 19, 0, 0, 0 );
 
-
+const timeGivenPreCalltime = minutesToMilliseconds(15);
+const timeGivenAfterWrap = minutesToMilliseconds(15);
 
 const millisecsInMinute = 60000; 
-console.log("Date is " + d);
-console.log("Calltime was: " + calltime);
-const justDate = new Date();
-console.log("Just date " + justDate.getDate());
+const millisecsInHour = 3600000;
 
+console.log("Calltime was: " + calltime);
+
+// this converts milliseconds to minutes
 function millisecondsToMinutes(milliseconds){
     minutes = milliseconds / millisecsInMinute;
     return minutes;
 }
 
+// this converts milliseconds to hours
 function millisecondsToHours(milliseconds){
-    hours = milliseconds / 3600000;
+    hours = milliseconds / millisecsInHour;
     return hours;
+}
+
+// this converts minutes to milliseconds
+function minutesToMilliseconds(minutes){
+    let milliseconds = minutes * 60000;
+    return milliseconds;
 }
 
 function calculateHoursWorked(){
     const hoursWorked = finishedWork - beganWork;
     return hoursWorked;
+}
+
+function calculateHoursWorkedMinusGivenTime() {
+    const hoursWorkedMinusGivenTime = finishedWork - beganWork - timeGivenAfterWrap - timeGivenPreCalltime;
+    return hoursWorkedMinusGivenTime;
 }
 
 function calculateOnCameraHours(){
@@ -41,7 +55,8 @@ function calculatePreCall(calltime){
     return preCall;
 }
 
-console.log("Hours worked was " + millisecondsToHours(calculateHoursWorked()));
+console.log("Total hours worked was " + millisecondsToHours(calculateHoursWorked()));
+console.log("Total hours minus given time = " + millisecondsToHours(calculateHoursWorkedMinusGivenTime()));
 
 if(millisecondsToHours(calculateHoursWorked()) > millisecondsToHours(calculateOnCameraHours())){
     console.log("Overtime may be due");
