4d67a4d79d7b7ae8609cd9c6ea3ed8afcffc3425
Sarah McConville <sarah.mcconville@wegotpop.com>
2021-10-25 15:20:45 +0100

    added daytype enum & lunchtime switch

--- datetime.js
+++ datetime.js
@@ -9,32 +9,49 @@ const result = sub(new Date(2017, 5, 15, 15, 29, 20), {
     hours: 5,
     minutes: 9,
     seconds: 30
-  }) */
+  }) 
 
-console.log(result);
-const calltime = new Date(2021, 9, 25, 8, 0, 0, 0);
-const wrap = new Date(2021,9, 25, 19, 0, 0, 0 );
+console.log(result);*/
 const beganWork = new Date(2021, 9, 25, 7, 30, 0, 0);
 const finishedWork = new Date(2021, 9, 25, 20, 0, 0, 0);
+const calltime = new Date(2021, 9, 25, 8, 0, 0, 0);
+const wrap = new Date(2021,9, 25, 19, 0, 0, 0 );
 const expectedWrap = new Date(2021,9, 25, 19, 0, 0, 0 );
+
+const dayTypes = {
+    CWD: "continuousWorkingDay",
+    SCWD: "semiContinuousWorkingDay",
+    SWD: "standardWorkingDay",
+}
+
+let lunchbreak = undefined;
+let typeOfDay = dayTypes.SWD;
+switch (typeOfDay) {
+    case dayTypes.CWD:
+        lunchbreak = minutesToMilliseconds(0);
+    case dayTypes.SCWD:
+        lunchbreak = minutesToMilliseconds(30);
+    case dayTypes.SWD:
+        lunchbreak = minutesToMilliseconds(60);    
+}
 
 const timeGivenPreCalltime = minutesToMilliseconds(15);
 const timeGivenAfterWrap = minutesToMilliseconds(15);
 
-const millisecsInMinute = 60000; 
-const millisecsInHour = 3600000;
+const millisecondsInMinute = 60000; 
+const millisecondsInHour = 3600000;
 
 console.log("Calltime was: " + calltime);
 
 // this converts milliseconds to minutes
 function millisecondsToMinutes(milliseconds){
-    minutes = milliseconds / millisecsInMinute;
+    minutes = milliseconds / millisecondsInMinute;
     return minutes;
 }
 
 // this converts milliseconds to hours
 function millisecondsToHours(milliseconds){
-    hours = milliseconds / millisecsInHour;
+    hours = milliseconds / millisecondsInHour;
     return hours;
 }
 
@@ -44,15 +61,15 @@ function minutesToMilliseconds(minutes){
     return milliseconds;
 }
 
-
+// this calculates the hours worked
 function calculateHoursWorked(){
-    const hoursWorked = finishedWork - beganWork;
-    return hoursWorked;
+    const hoursWorked = finishedWork - beganWork - lunchbreak;
+    return millisecondsToHours(hoursWorked);
 }
 
 function calculateHoursWorkedMinusGivenTime() {
     const hoursWorkedMinusGivenTime = finishedWork - beganWork - timeGivenAfterWrap - timeGivenPreCalltime;
-    return hoursWorkedMinusGivenTime;
+    return millisecondsToHours(hoursWorkedMinusGivenTime);
 }
 
 function calculateOnCameraHours(){
@@ -68,11 +85,13 @@ function calculatePreCall(calltime){
     return preCall;
 }
 
-console.log("Total hours worked was " + millisecondsToHours(calculateHoursWorked()));
-console.log("Total hours minus given time = " + millisecondsToHours(calculateHoursWorkedMinusGivenTime()));
+console.log("Total hours worked was " + calculateHoursWorked());
+console.log("Total hours minus given time = " + calculateHoursWorkedMinusGivenTime());
 
-if(millisecondsToHours(calculateHoursWorked()) > millisecondsToHours(calculateOnCameraHours())){
+if(calculateHoursWorked() > calculateOnCameraHours()){
     console.log("Overtime may be due");
+} else {
+    console.log("Overtime not due");
 }
 
 console.log("Precall was " + calculatePreCall(calltime) + " minutes");
