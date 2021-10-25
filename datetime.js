d78a01cc6be45530b488eb0c7bdf7afa5578108e
Sarah McConville <sarah.mcconville@wegotpop.com>
2021-10-25 10:56:36 +0100

    includes millisecondsToMinutes function

--- datetime.js
+++ datetime.js
@@ -3,12 +3,18 @@ const calltime = new Date(2021, 9, 25, 8, 0, 0, 0);
 console.log("Date is " + d);
 console.log("Calltime was: " + calltime);
 
+function millisecondsToMinutes(milliseconds){
+    minutes = milliseconds / 60000;
+    return minutes;
+}
+
 function calculatePreCall(calltime){
     const beganWork = new Date(2021, 9, 25, 7, 30, 0, 0);
-    console.log("Given time " + givenTime);
-    const preCall = calltime - beganWork;
+    //console.log("Given time " + givenTime);
+    let preCall = calltime - beganWork;
+    preCall = millisecondsToMinutes(preCall);
     return preCall;
 }
 
 
-console.log("Precall was " + calculatePreCall(calltime));
+console.log("Precall was " + calculatePreCall(calltime) + " minutes");
