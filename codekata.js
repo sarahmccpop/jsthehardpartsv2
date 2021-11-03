f0dac249ab7213c3dfd690605cdd5397ef916af3
Sarah McConville <sarah.mcconville@wegotpop.com>
2021-11-03 17:31:23 +0000

    fearchar's much improved code

--- codekata.js
+++ codekata.js
@@ -1,31 +1,28 @@
 // https://www.codewars.com/kata/5b180e9fedaa564a7000009a/train/javascript
-const solve = (word) => {
-  let uppercaseNumber = 0;
-  let lowercaseNumber = 0;
-  const countAndConvert = (word) => {
-    let charAt = word.split("");
-    let uppercaseLetterCheck = /[A-Z]/g;
-    let newWord = "";
-    for (let i = 0; i < word.length; i++) {
-      const letterBeingChecked = charAt[i];
-      const numberOfUppercaseLettersFound =
-        letterBeingChecked.match(uppercaseLetterCheck);
-      if (numberOfUppercaseLettersFound) {
-        uppercaseNumber++;
-      }
-    }
-    lowercaseNumber = word.length - uppercaseNumber;
 
-    if (lowercaseNumber === uppercaseNumber) {
-      newWord = word.toLowerCase();
-    } else if (lowercaseNumber > uppercaseNumber) {
-      newWord = word.toLowerCase();
-    } else {
-      newWord = word.toUpperCase();
+function uppercaseNum(word) {
+    const splitWord = word.split("");
+
+    let uppercaseNumber = 0;
+    for (let i = 0; i < word.length; i++) {
+        const letterBeingChecked = splitWord[i];
+        const letterFound = letterBeingChecked.match(/[A-Z]/g);
+
+        if (letterFound) {
+            uppercaseNumber++;
+        }
     }
+
+    return uppercaseNumber
+}
+
+function solve(word) {
+    const uppercaseNumber = uppercaseNum(word)
+
+    const lowercaseNumber = word.length - uppercaseNumber;
+    const newWord = lowercaseNumber >= uppercaseNumber ? word.toLowerCase() : word.toUpperCase();
 
     return newWord;
-  };
-  return countAndConvert(word);
 };
+
 console.log("Name is ", solve("SArah"));
