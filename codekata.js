a64dbb8e850f29f04b1fcaaf5c71561d540f3f16
Sarah McConville <sarah.mcconville@wegotpop.com>
2021-11-03 16:21:59 +0000

    codewars challenge

---
+++ codekata.js
@@ -0,0 +1,30 @@
+const solve = (word) => {
+  let uppercaseNumber = 0;
+  let lowercaseNumber = 0;
+  const countAndConvert = (word) => {
+    let charAt = word.split("");
+    let uppercaseLetterCheck = /[A-Z]/g;
+    let newWord = "";
+    for (let i = 0; i < word.length; i++) {
+      const letterBeingChecked = charAt[i];
+      const numberOfUppercaseLettersFound =
+        letterBeingChecked.match(uppercaseLetterCheck);
+      if (numberOfUppercaseLettersFound) {
+        uppercaseNumber++;
+      }
+    }
+    lowercaseNumber = word.length - uppercaseNumber;
+
+    if (lowercaseNumber === uppercaseNumber) {
+      newWord = word.toLowerCase();
+    } else if (lowercaseNumber > uppercaseNumber) {
+      newWord = word.toLowerCase();
+    } else {
+      newWord = word.toUpperCase();
+    }
+
+    return newWord;
+  };
+  return countAndConvert(word);
+};
+console.log("Name is ", solve("SArah"));
