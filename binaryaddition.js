8088a4c8ccf2c7b7c77635e4bdd46e77ec40fe0c
Sarah McConville <sarah.mcconville@wegotpop.com>
2021-11-05 10:44:54 +0000

    2 additional codewars exercises added

---
+++ binaryaddition.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript
+function addBinary(a, b){
+    sum = a+b;
+    return sum.toString(2);
+}
+console.log(addBinary(1, 2));
+console.log(addBinary(3,9));
+console.log(addBinary(10,6));
+console.log(addBinary(51, 12));
---
+++ twotoone.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5656b6906de340bd1b0000ac/javascript
+function longest(s1, s2) {
+    // remove duplicates from strings
+    let s1Array = s1.toLowerCase().split('') +  "," + s2.toLowerCase().split('');
+    // let s2Array = s2.toLowerCase().split('');
+   // s1Array.push(...s2Array);
+  let uniq = [...new Set(s1Array)];
+
+
+    return uniq;
+  }
+  console.log(longest("Sarah","Harry"));
